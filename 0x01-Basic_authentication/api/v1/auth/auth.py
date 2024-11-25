2471e81fdaecf9d7c8f9c141634186c19b9bc924
lafertd <achrafprofessionalinfo@gmail.com>
2024-11-25 01:55:59 +0100

    updated

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,6 +6,8 @@ from flask import request
 from typing import List, TypeVar
 
 class Auth:
+    """  Auth: Class to manage the API authentication
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         return False
     def authorization_header(self, request=None) -> str:
