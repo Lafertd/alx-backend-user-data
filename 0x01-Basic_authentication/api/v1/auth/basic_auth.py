2a98121758e968aa637d7aa6eabfe4c2e1e370e8
lafertd <achrafprofessionalinfo@gmail.com>
2024-11-25 16:02:30 +0100

    updated

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
-from auth import Auth
+from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
+    """
+    BasicAuth class that inherits from Auth.
+    Currently, it does not implement any specific functionality.
+    """
     pass
+
