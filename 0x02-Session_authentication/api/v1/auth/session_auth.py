a8d087bdcf40c7fc870a52ee8e208ef97109faf2
lafertd <achrafprofessionalinfo@gmail.com>
2024-11-28 14:43:21 +0100

    updated

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,4 +3,6 @@ from api.v1.auth.auth import Auth
 """ Session Authentication Module
 """
 class SessionAuth(Auth):
+    """ Class that serves as the base for session authentication
+    """
     pass
