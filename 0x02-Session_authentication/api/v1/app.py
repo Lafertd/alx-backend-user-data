78908ac9fb3f4a0e18c9a26ed75ebe43745085a8
lafertd <achrafprofessionalinfo@gmail.com>
2024-11-28 20:57:06 +0100

    updated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -41,34 +41,33 @@ def forbidden(error) -> str:
     """
     return jsonify({"error": "Forbidden"}), 403
 
-@app.before_request
+@ app.before_request
 def before_request() -> str:
+    """ Before Request Handler
+    Requests Validation
     """
-    Filter each request before it's handled by the proper route
-    """
-    if request is None:
-        return None
-
-    excluded_paths = [
-            '/api/v1/status/', 
-            '/api/v1/unauthorized/', 
-            '/api/v1/forbidden/', 
-            '/api/v1/auth_session/login/']
-
-    If auth.authorization_header(request) is None and auth.session_cookie(request) None:
-        abort(401)
-
-    if auth is None or not auth.require_auth(request.path, excluded_paths):
+    if auth is None:
         return
 
-    if auth.authorization_header(request) is None:
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
 
-    if auth.current_user(request) is None:
+    current_user = auth.current_user(request)
+    if current_user is None:
         abort(403)
 
-    request.current_user = auth.current_user(request))
-if __name__ == "__main__":
+    request.current_user = current_user
+
+    if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(debug=True, host=host, port=port)
